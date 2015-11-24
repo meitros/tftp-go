53875843f37f85cd608014149cee4005c1af59e2
Mihir Kedia <meitros@gmail.com>
2015-11-23 20:30:53 -0500

    gofmt -w

--- tftpd.go
+++ tftpd.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"time"
-	"io"
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"strings"
+	"time"
 )
 
 type block struct {
 	blocknum int
-	nbytes int
-	buf []byte 
+	nbytes   int
+	buf      []byte
 }
+
 var debugflag bool
 var chunk block
 var ReadMode bool
@@ -31,9 +32,9 @@ func DieOnError(err error) {
 func debugPrint(msg string) {
 	if debugflag {
 		fmt.Println(msg)
-		}
 	}
-	
+}
+
 // finds a null byte in n, or -1 if not present
 func findNull(n []byte) int {
 	for i := 0; i < len(n); i++ {
@@ -56,7 +57,7 @@ func parsePacket(packet []byte) (int, map[string][]byte, error) {
 		opcodeName := map[int]string{1: "RRQ", 2: "WRQ"}[opcode]
 
 		if ReadMode && opcode != 1 {
-			fmt.Println("The opcode received was unexpected" )
+			fmt.Println("The opcode received was unexpected")
 		}
 		// grab the filename and mode strings
 		firstNull := findNull(packet[2:])
@@ -79,13 +80,13 @@ func parsePacket(packet []byte) (int, map[string][]byte, error) {
 		mode := packet[firstNull+1 : secondNull]
 
 		// set the transferMode variable
-/*		if bytes.Equal(mode,[]byte("NetAscii")) {
-			transferMode =NETASCII
-		} 
-		transferMode = NETASCII
-		if byies.Equal(mode,[]byte("Binary")) {
-			transferMode = BINARY
-		} */
+		/*		if bytes.Equal(mode,[]byte("NetAscii")) {
+					transferMode =NETASCII
+				}
+				transferMode = NETASCII
+				if byies.Equal(mode,[]byte("Binary")) {
+					transferMode = BINARY
+				} */
 		fields := map[string][]byte{
 			"filename": filename,
 			"mode":     mode,
@@ -98,14 +99,14 @@ func parsePacket(packet []byte) (int, map[string][]byte, error) {
 			"blocknum": packet[2:4],
 			"data":     packet[4:],
 		}
-		return 3,fields, nil
+		return 3, fields, nil
 	case 4:
 		// Ack
 		// TODO: robustness
 		fields := map[string][]byte{
 			"blocknum": packet[2:4],
 		}
-		return 4,fields, nil
+		return 4, fields, nil
 	case 5:
 		// Error
 		nullPos := findNull(packet[4:])
@@ -146,55 +147,56 @@ func sendAck(conn *net.UDPConn, addr *net.UDPAddr, blocknum []byte) {
 	var ack = "\x00\x04"
 	//	insertBlockNum(ack[2], blocknum)
 	var res = append([]byte(ack), blocknum...)
-	fmt.Println(res,"Sending ack code ",ack,"blocknum ",blocknum)
+	fmt.Println(res, "Sending ack code ", ack, "blocknum ", blocknum)
 	_, err := conn.WriteToUDP(res, addr)
 	if err != nil {
 		fmt.Printf("Couldn't send response %v", err)
 	}
 }
 
-func SendAChunk(conn *net.UDPConn, addr *net.UDPAddr){
+func SendAChunk(conn *net.UDPConn, addr *net.UDPAddr) {
 	var datablock []byte
 	//Send the chink that we have in the Block (it is either the previous one being retransmitted or a the next one
-	var opcode = "\x00\x03"   //Data packet
+	var opcode = "\x00\x03" //Data packet
 	/*
-	var nullbyte = "\x00"
-	s16 := strconv.FormatUint(uint64(chunk.blocknum), 16)
-	fmt.Println("=========== blocknum and conv",uint64(chunk.blocknum),"   ", s16)
-	if chunk.blocknum < 16 {
-		fmt.Println("Padding a zero byte,before zero",datablock[0:])
-		datablock = append([]byte(opcode), []byte(nullbyte)...) 
-		fmt.Println("Padding a zero byte, after zero",datablock[0:])
-	}
-	sbytes := []byte(s16)
-	fmt.Println(sbytes)
-	fmt.Println("Adding the blocknum, datablock before num",datablock[0:])
-	datablock = append([]byte(opcode), []byte(s16)...) 
-	fmt.Println("Adding the blocknum, datablock after num",datablock[0:])
-	datablock = append(datablock,chunk.buf...)
+		var nullbyte = "\x00"
+		s16 := strconv.FormatUint(uint64(chunk.blocknum), 16)
+		fmt.Println("=========== blocknum and conv",uint64(chunk.blocknum),"   ", s16)
+		if chunk.blocknum < 16 {
+			fmt.Println("Padding a zero byte,before zero",datablock[0:])
+			datablock = append([]byte(opcode), []byte(nullbyte)...)
+			fmt.Println("Padding a zero byte, after zero",datablock[0:])
+		}
+		sbytes := []byte(s16)
+		fmt.Println(sbytes)
+		fmt.Println("Adding the blocknum, datablock before num",datablock[0:])
+		datablock = append([]byte(opcode), []byte(s16)...)
+		fmt.Println("Adding the blocknum, datablock after num",datablock[0:])
+		datablock = append(datablock,chunk.buf...)
 	*/
 	var blocknumBytes = make([]byte, 2)
 	binary.BigEndian.PutUint16(blocknumBytes, uint16(chunk.blocknum))
-	
-	datablock = append(datablock, []byte(opcode)...);
-	datablock = append(datablock, blocknumBytes...);
-	fmt.Println("Stage 2: Datablock ",datablock)
-	if (chunk.nbytes < 512) {
+
+	datablock = append(datablock, []byte(opcode)...)
+	datablock = append(datablock, blocknumBytes...)
+	fmt.Println("Stage 2: Datablock ", datablock)
+	if chunk.nbytes < 512 {
 		// make a slice of size chunk.nbytes and copy the data into it
 		tempbuf := make([]byte, chunk.nbytes)
 		n1 := copy(tempbuf, chunk.buf[0:chunk.nbytes])
-		fmt.Println("Copied %d bytes to the last chunk being sent",n1)
-		datablock = append(datablock,tempbuf...)
+		fmt.Println("Copied %d bytes to the last chunk being sent", n1)
+		datablock = append(datablock, tempbuf...)
 	} else {
-		datablock = append(datablock,chunk.buf...)
-		}
-	fmt.Println("sending datablock",datablock)
-	n2, err := conn.WriteToUDP(datablock,addr)
-	fmt.Println("Number of Bytes sent is ",n2)
+		datablock = append(datablock, chunk.buf...)
+	}
+	fmt.Println("sending datablock", datablock)
+	n2, err := conn.WriteToUDP(datablock, addr)
+	fmt.Println("Number of Bytes sent is ", n2)
 	if err != nil {
 		fmt.Printf("Couldn't send datablock %v", err)
 	}
 }
+
 /*
 func LoadFile(filename string,transferMode string) *os.File{
     // open input file
@@ -221,24 +223,30 @@ func LoadFile(filename string,transferMode string) *os.File{
 	return fi
 }
 */
-func ReadAChunk(inFile *os.File) int{
+func ReadAChunk(inFile *os.File) int {
 	// make a buffer to keep chunks that are read
-	if LastBlockSent {return 0}
+	if LastBlockSent {
+		return 0
+	}
 	LastBlockSent = false
 	chunk.blocknum++
 	if chunk.blocknum > 0 {
 		mybuf := make([]byte, 512)
-	    // read a chunk
-		fmt.Println("About to read Block num ",chunk.blocknum,"the handle is",inFile,"END OF HANDLE***************************")
-	    n, err := inFile.Read(mybuf)
-	    if err != nil && err != io.EOF {
-	        panic(err)
-	    }
+		// read a chunk
+		fmt.Println("About to read Block num ", chunk.blocknum, "the handle is", inFile, "END OF HANDLE***************************")
+		n, err := inFile.Read(mybuf)
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
 		chunk.nbytes = n
-	    chunk.buf = mybuf
-		if n < 512 {LastBlockSent = true}
+		chunk.buf = mybuf
+		if n < 512 {
+			LastBlockSent = true
+		}
 		return n
-	} else {return 512}
+	} else {
+		return 512
+	}
 }
 
 func main() {
@@ -246,7 +254,9 @@ func main() {
 	var err error
 	port := os.Args[1]
 	debugflag = false
-	if strings.Compare(os.Args[2],"debug") == 0 {debugflag = true}
+	if strings.Compare(os.Args[2], "debug") == 0 {
+		debugflag = true
+	}
 	ReadMode = false
 	fmt.Println("listening on port " + port)
 
@@ -272,14 +282,14 @@ func main() {
 		}
 		if err != nil {
 			e, ok := err.(net.Error)
-	        if !ok || !e.Timeout() {
-	            // handle error, it's not a timeout
+			if !ok || !e.Timeout() {
+				// handle error, it's not a timeout
 				fmt.Println("Error: ", err)
 				continue
-			} 
+			}
 			if e.Timeout() {
-			//We timed out retransmit the block
-			SendAChunk(ServerConn,raddr)				
+				//We timed out retransmit the block
+				SendAChunk(ServerConn, raddr)
 			}
 		}
 		opcode, fields, err := parsePacket(buf[0:n])
@@ -291,40 +301,40 @@ func main() {
 
 		if opcode == 1 {
 			debugPrint("Processing opcode 1")
-//			inFile = LoadFile(string(fields["filename"]), string(fields["mode"]))
-		    inFile, err = os.Open(string(fields["filename"]))
-		    if err != nil {
-		        panic(err)
-		    }
-		    // close fi on exit and check for its returned error
-		    defer func() {
-		        if err := inFile.Close(); err != nil {
-		            panic(err)
-		        }
-		    }()
+			//			inFile = LoadFile(string(fields["filename"]), string(fields["mode"]))
+			inFile, err = os.Open(string(fields["filename"]))
+			if err != nil {
+				panic(err)
+			}
+			// close fi on exit and check for its returned error
+			defer func() {
+				if err := inFile.Close(); err != nil {
+					panic(err)
+				}
+			}()
 			debugPrint("File loaded, going to read chunk")
 			n := ReadAChunk(inFile)
-			fmt.Println("Read chunk with ",n," bytes")
+			fmt.Println("Read chunk with ", n, " bytes")
 			SendAChunk(ServerConn, raddr)
 		}
 		if opcode == 2 {
 			debugPrint("Processing opcode 2")
 			sendAck(ServerConn, raddr, []byte(blocknum))
-		} 
+		}
 		if opcode == 4 {
-//we are skipping check of block number for now, need to fix it for robustness
-//			if string(fields[blocknum]) == string(chunk.blocknum) {
+			//we are skipping check of block number for now, need to fix it for robustness
+			//			if string(fields[blocknum]) == string(chunk.blocknum) {
 			debugPrint("Processing opcode 4")
 			fmt.Println()
 			n := ReadAChunk(inFile)
-			fmt.Println("Read chunk with ",n," bytes")
-			if (n > 0) {
+			fmt.Println("Read chunk with ", n, " bytes")
+			if n > 0 {
 				SendAChunk(ServerConn, raddr)
 			} else {
 				fmt.Println("File is all transmitted")
 				os.Exit(0)
 			}
-//				}
+			//				}
 		}
 	}
 }
